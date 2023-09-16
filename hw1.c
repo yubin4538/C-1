564d4f2c23e52880f891030d73529bbf5aa94c14
yubin4538 <202110097@sangmyung.kr>
2023-09-16 20:02:48 +0900

    Add C Source File [hw1.c]

---
+++ hw1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num1, num2;
+	printf("Input two integers: ");
+	scanf_s("%d %d", &num1, &num2);
+	printf("%d & %d = %d \n", num1, num2, num1&num2);
+	printf("%d | %d = %d \n", num1, num2, num1 | num2);
+	printf("%d ^ %d = %d \n", num1, num2, num1 ^ num2);
+	return 0;
+}
